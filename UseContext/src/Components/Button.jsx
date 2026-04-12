11a23e17f8ef05544171b9170dac65e9dd3f8b96
Inchara nk <incharank1@gmail.com>
2026-04-12 22:06:12 +0530

    Intro to useContext

---
+++ UseContext/src/Components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Component1 from './Component1'
+
+const button = () => {
+  return (
+    <div>
+     <button><span><Component1/></span>I am a Button</button>
+    </div>
+  )
+}
+export default button
